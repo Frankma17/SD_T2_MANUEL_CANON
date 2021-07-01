e13b15b6b6d905113180e37e3387471f793d5ce6
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2021-07-01 17:19:57 -0500

    Add files via upload

---
+++ server_express.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app  = express()
+let names = ["Pedro","Juan","Lucas"]
+
+app.get("/",(req,res)=> {
+    position = req.query.size
+    result = names[position];
+    res.send("En la posicion " + position 
+    + " se encuentra el usuario " + result)
+    console.log(result)
+})
+
+app.listen(5000,()=>{
+    console.log("Recibido en el puerto 5000")
+})
+
+
